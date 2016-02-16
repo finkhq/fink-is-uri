ff522f297f602d8214801202f4282834faf91aa6
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-16 13:33:27 +0100

    avoid const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const _isURI = require('is-uri')
-const parseURI = require('parse-uri')
+var _isURI = require('is-uri')
+var parseURI = require('parse-uri')
 
 module.exports = function isURI (uri, hostname) {
   uri = parseURI(uri)
